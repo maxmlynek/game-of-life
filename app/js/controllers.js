4ae882ed9eddd43db3b9332e07d6866853f3cccf
max <maxmlynek@gmail.com>
2016-05-23 17:41:52 +0200

    add table

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,3 +1,16 @@
 /**
  * Created by Max on 5/23/2016.
  */
+angular.module('app',[]).controller('GameOfLifeController', GameOfLifeController);
+
+
+
+function GameOfLifeController($scope){
+
+    $scope.initializeGame = function (){
+        $scope.field =  new Array(60).fill(new Array(100).fill());
+    }
+    $scope.initializeGame();
+}
+
+
