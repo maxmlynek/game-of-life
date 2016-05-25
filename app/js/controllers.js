46f02106ba26f42c585a95cfad5b0fe56ec9cb3f
max <maxmlynek@gmail.com>
2016-05-25 17:39:29 +0200

    add field changing after clicking

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,15 +3,69 @@
  */
 angular.module('app',[]).controller('GameOfLifeController', GameOfLifeController);
 
+var rowOfField = new Array(100).fill(0);
+var field = new Array(60);
+for(var i = 0; i<field.length; i++){
+    field[i] = rowOfField.slice();
+}
 
+
+//Controller
 
 function GameOfLifeController($scope){
 
     $scope.initializeGame = function (){
-        $scope.field =  new Array(60).fill(new Array(100).fill());
+        $scope.field = field;
     }
 
     $scope.initializeGame();
 }
 
 
+
+
+
+var changeField = function(indexes){
+    var indices = indexes.split(".");
+    var i = parseInt(indices[0]);
+    var j = parseInt(indices[1]);
+    if(field[i][j] == 0) field[i][j] = 1;
+    else field[i][j] = 0;
+}
+
+//jQuery
+
+$(document).ready(function () {
+
+    var down = false;
+    $(document).mousedown(function () {
+        down = true;
+    }).mouseup(function () {
+        down = false;
+    });
+
+    $('td').mousedown(function () {
+        if($(this).css('background-color')=='rgb(140, 140, 140)'){
+            $(this).css('background-color', 'darkgreen');
+            changeField($(this).text());
+        } else {
+            $(this).css('background-color', '#8c8c8c');
+            changeField($(this).text());
+        }
+    });
+
+    $("td").mousemove(function () {
+        if (down) {
+            if($(this).css('background-color')=='rgb(140, 140, 140)'){
+                $(this).css('background-color', 'darkgreen');
+                changeField($(this).text());
+            } else {
+                $(this).css('background-color', '#8c8c8c');
+                changeField($(this).text());
+            }
+        }
+
+    });
+
+
+});
