2545523d9f6cbeb13f3bfbfbbc72b35d36e2dcb0
max <maxmlynek@gmail.com>
2016-05-25 23:46:11 +0200

    add logic

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,22 +3,114 @@
  */
 angular.module('app', []).controller('GameOfLifeController', GameOfLifeController);
 
+var isStarted = false;
+
 var rowOfField = new Array(100).fill(0);
 var field = new Array(60);
 for (var i = 0; i < field.length; i++) {
     field[i] = rowOfField.slice();
 }
 
+var countingArrayRow = new Array(rowOfField.length).fill(0);
+var countingArray = new Array(field.length);
+for (var i = 0; i < countingArray.length; i++) {
+    countingArray[i] = countingArrayRow.slice();
+}
+
 var cleaner = function(){
     $('td').removeAttr('style');
 }
 
+var countNeighbours = function(row, col){
+    //Michael Laszlo algorithm
+    var count = 0;
+    for (var i = row - 1; i <= row + 1; i++) {
+        for (var j = col - 1; j <= col + 1; j++) {
+            if (i == row && j == col) {
+                continue;
+            }
+            count += field[(i + field.length) % field.length][(j + field[0].length) % field[0].length];
+        }
+    }
+    return count;
+}
+
+var validateArrays = function(){ //validate if lifeArray and deathArray are distinct
+    return true;
+}
+
+
+
 //Controller
 
 
-function GameOfLifeController($scope) {
+function GameOfLifeController($scope, $interval) {
+
+    $scope.isPaused =false;
+    $scope.lifeArray = [ false, false, false,
+                        true, false, false,
+                        false, false, false];
+
+    $scope.deathArray= [ true, true, false,
+                     false, true, true,
+                        true, true, true];
+
+    $scope.timeSet = {
+        time: 'c'
+    }
+
+    $scope.countTime = function(){
+        if($scope.timeSet.time == 'a') return 200;
+        if($scope.timeSet.time == 'b') return 400;
+        if($scope.timeSet.time == 'c') return 600;
+        if($scope.timeSet.time == 'd') return 800;
+        if($scope.timeSet.time == 'e') return 1000;
+    }
+
+    $scope.startGame = function(){
+
+        isStarted = true;
+        $scope.isPaused = false;
+
+        $interval( function() {
+
+                if(!$scope.isPaused) {
+                    validateArrays()//for every iteration!
+                    var i, j;
+                    for (i = 0; i < countingArray.length; i++) {
+                        for (j = 0; j < countingArray[0].length; j++) {
+                            countingArray[i][j] = countNeighbours(i, j);
+                        }
+                    }
+
+                    for (i = 0; i < field.length; i++) {
+                        for (j = 0; j < field[0].length; j++) {
+                            if ($scope.lifeArray[countingArray[i][j]] == true) {
+                                field[i][j] = 1;
+                            } else if ($scope.deathArray[countingArray[i][j]] == true) {
+                                field[i][j] = 0;
+                            }
+
+                        }
+                    }
+                    cleaner();
+                    $scope.initializeGame();
+                }
+            },
+
+            $scope.countTime()
+
+        );
+
+
+    };
+
+    $scope.stopGame = function(){
+        $scope.isPaused = true;
+    };
 
     $scope.initializeGame = function () {
+
         $scope.field = field;
     };
 
@@ -50,6 +142,7 @@ function GameOfLifeController($scope) {
         }
 
         $scope.initializeGame();
+        isStarted = false;
     }
 
     $scope.initializeGame();
--- app/js/script.js
+++ app/js/script.js
@@ -2,17 +2,17 @@
  * Created by Max on 5/25/2016.
  */
 
-var changeField = function(indexes){
+var changeField = function (indexes) {
     var indices = indexes.split(".");
     var i = parseInt(indices[0]);
     var j = parseInt(indices[1]);
-    if(field[i][j] == 0) field[i][j] = 1;
+    if (field[i][j] == 0) field[i][j] = 1;
     else field[i][j] = 0;
 }
 
 
-
 $(document).ready(function () {
+
 
     var down = false;
     $(document).mousedown(function () {
@@ -22,24 +22,34 @@ $(document).ready(function () {
     });
 
     $('td').mousedown(function () {
-        if($(this).css('background-color')=='rgb(140, 140, 140)'){
-            $(this).css('background-color', 'darkgreen');
-            changeField($(this).text());
-        } else {
-            $(this).css('background-color', '#8c8c8c');
-            changeField($(this).text());
-        }
-    });
 
-    $("td").mousemove(function () {
-        if (down) {
-            if($(this).css('background-color')=='rgb(140, 140, 140)'){
+        if(!isStarted) {
+
+            if ($(this).css('background-color') == 'rgb(140, 140, 140)') {
                 $(this).css('background-color', 'darkgreen');
                 changeField($(this).text());
             } else {
                 $(this).css('background-color', '#8c8c8c');
                 changeField($(this).text());
             }
+
+        }
+    });
+
+    $("td").mousemove(function () {
+
+        if(!isStarted) {
+
+            if (down) {
+                if ($(this).css('background-color') == 'rgb(140, 140, 140)') {
+                    $(this).css('background-color', 'darkgreen');
+                    changeField($(this).text());
+                } else {
+                    $(this).css('background-color', '#8c8c8c');
+                    changeField($(this).text());
+                }
+            }
+
         }
 
     });
